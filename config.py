151da9d52dddf93edd1f099ee1a720a69d1cf9a4
Oleksandr <tryxer2302@gmail.com>
2025-06-02 12:33:26 +0300

    init

---
+++ config.py
@@ -0,0 +1,24 @@
+STUDENT_ID = "Bilokin_87ce13b3"
+SOURCES = [
+    "http://feeds.bbci.co.uk/news/rss.xml",
+    "http://feeds.reuters.com/reuters/topNews",
+    "https://www.theguardian.com/world/rss",
+    "https://www.aljazeera.com/xml/rss/all.xml",
+    "https://apnews.com/rss",
+    "https://khanacademy.org/about/blog?format=rss",
+    "https://blog.edx.org/feed",
+    "https://blog.coursera.org/feed/",
+    "http://news.mit.edu/rss/topic/education",
+    "https://news.stanford.edu/feed/",
+    "https://www.sciencedaily.com/rss/all.xml",
+    "https://www.nature.com/nature.rss",
+    "https://arxiv.org/rss/astro-ph",
+    "https://www.nasa.gov/rss/dyn/breaking_news.rss",
+    "https://www.nationalgeographic.com/content/natgeo/en_us/index.rss",
+    "https://stackoverflow.blog/feed/",
+    "https://github.blog/feed/",
+    "https://hacks.mozilla.org/feed/",
+    "http://feeds.feedburner.com/TechCrunch/",
+    "https://hnrss.org/frontpage"
+]
+
